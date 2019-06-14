50b966c1b230e78ad38e38a45d17d2c8f3f7917e
iven00000000 <iven00000000@gmail.com>
2019-06-14 19:34:23 +0800

    Test boundary cases of `atobi()`

--- test.cpp
+++ test.cpp
@@ -22,4 +22,49 @@ TEST(atobi, HandleZeros) {
   }
 }
 
+TEST(atobi, Boundary) {
+  Bigint* n;
+  
+  n = atobi("1");
+  EXPECT_EQ(n->chunks[0], 1);
+  EXPECT_EQ(n->len, 1);
+  EXPECT_EQ(n->bits, 1);
+  delete n->chunks;
+  delete n;
+
+  n = atobi("3");
+  EXPECT_EQ(n->chunks[0], 3);
+  EXPECT_EQ(n->len, 1);
+  EXPECT_EQ(n->bits, 2);
+  delete n->chunks;
+  delete n;
+
+  unsigned long long num = (~0ULL) >> 1; // num = 0b0111...111
+  int cnt = snprintf(NULL, 0, "%llu", num);
+  // 2^k-1 never be the format of 999...999, 
+  // so reserving cnt+1 bytes is enough
+  char* s = new char[cnt+1]; 
+
+  snprintf(s, cnt+1, "%llu", num);
+  n = atobi(s);
+  EXPECT_GE(n->len, 1);
+  EXPECT_EQ(n->chunks[0], num);
+  EXPECT_EQ(n->bits, sizeof(unsigned long long)*8-1);
+  delete n->chunks;
+  delete n;
+
+  ++num; // num = 0b1000...000
+
+  snprintf(s, cnt+1, "%llu", num);
+  n = atobi(s);
+  EXPECT_GE(n->len, 2);
+  EXPECT_EQ(n->chunks[0], 0);
+  EXPECT_EQ(n->chunks[1], 1);
+  EXPECT_EQ(n->bits, sizeof(unsigned long long)*8);
+  delete n->chunks;
+  delete n;
+
+  delete s;
+}
+
 }
